c8497a9839d18fbc7876fb9044582e5ba3390c1f
Tyler <tylodonnell@gmail.com>
2013-11-23 00:34:06 -0500

    added date data type

--- models/wish.js
+++ models/wish.js
@@ -3,7 +3,8 @@ function init(Schema, mongoose){
 	var TheSchema = new Schema({
 		name: String,
 		message: String,
-        email: String
+        email: String,
+        created: new Date()
 	});
 
 	return mongoose.model('Wish', TheSchema);
