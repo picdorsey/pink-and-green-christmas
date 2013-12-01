7fd9ee53ef68a283f34bebf10443535c74dc7ea2
Tyler <tylodonnell@gmail.com>
2013-11-30 21:44:11 -0500

    authentication

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
 var express = require('express'),
-  http = require('http'),
-  path = require('path'),
-  io = require('socket.io'),
-  mongoose = require('mongoose'),
-  passport = require('passport'),
-  LocalStrategy = require('passport-local').Strategy,
-  passportLocalMongoose = require('passport-local-mongoose'),
-  Wish = require('./models/wish.js'),
-  Account = require('./models/account');
+    http = require('http'),
+    path = require('path'),
+    io = require('socket.io'),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    flash = require('connect-flash'),
+    LocalStrategy = require('passport-local').Strategy,
+    passportLocalMongoose = require('passport-local-mongoose'),
+    Wish = require('./models/wish.js'),
+    Account = require('./models/account');
 
 var app = express();
 
 // configuration
 app.configure(function(){
-  app.engine('.html', require('ejs').__express);
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'html');
-  app.use(express.compress());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'thisismysupersecret' }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(express.favicon());
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use('/assets', express.static(path.join(__dirname, 'public')));
+    app.engine('.html', require('ejs').__express);
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'html');
+    app.use(express.compress());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: 'thisismysupersecret' }));
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(express.favicon());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use('/assets', express.static(path.join(__dirname, 'public')));
 });
 
 // connect to DB
@@ -42,7 +44,7 @@ passport.deserializeUser(Account.deserializeUser());
 
 // start the server
 var server = http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+    console.log("Express server listening on port " + app.get('port'));
 });
 
 // sockets
