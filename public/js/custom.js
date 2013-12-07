d6fd0881387ceadf0b4d08274f96bc2320dee241
Tyler <tylodonnell@gmail.com>
2013-12-06 23:05:44 -0500

    added scroll to

--- public/js/custom.js
+++ public/js/custom.js
@@ -121,3 +121,22 @@ jQuery.fn.scaleRotate = function (offset, degrees) {
         'transform': 'scale(' + offset + ', ' + offset + ') ' + 'rotate(' + degrees + 'deg)'
     });
 };
+
+// scrollTo Plugin
+$.fn.scrollTo = function( target, options, callback ){
+  if(typeof options == 'function' && arguments.length == 2){ callback = options; options = target; }
+  var settings = $.extend({
+    scrollTarget  : target,
+    offsetTop     : 400,
+    duration      : 500,
+    easing        : 'swing'
+  }, options);
+  return this.each(function(){
+    var scrollPane = $(this);
+    var scrollTarget = (typeof settings.scrollTarget == "number") ? settings.scrollTarget : $(settings.scrollTarget);
+    var scrollY = (typeof scrollTarget == "number") ? scrollTarget : scrollTarget.offset().top + scrollPane.scrollTop() - parseInt(settings.offsetTop);
+    scrollPane.animate({scrollTop : scrollY }, parseInt(settings.duration), settings.easing, function(){
+      if (typeof callback == 'function') { callback.call(this); }
+    });
+  });
+}
